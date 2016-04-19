717724011c2d912004017e8b0181f3ea961655e7
zippo-yap <zippo-yap@gmail.com>
2016-04-19 12:06:48 +1200

    Create work object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -15,6 +15,25 @@ var bio = {
   "display": function () {}
 };
 
+var work = {
+  "jobs": [
+    {
+      "employer": "The College of Law",
+      "title": "Assistant to Director of Programmes",
+      "location": "Auckland",
+      "dates": "June 2014 - December 2015",
+      "description": "Assisting the Director in the administration and support of all New Zealand programmes. This role is primarily an administrative support role, which involves a significant IT and information management component."
+    },
+    {
+      "employer": "The College of Law",
+      "title": "Student Services Officer",
+      "location": "Auckland",
+      "dates": "June 2012 – June 2014",
+      "description": "Key member of the Student Services team contributing to the delivery of excellent customer service. Front desk position with responsibilities in assisting with student enrolments and administration, campus operations and events and marketing. Involved in the re-design, implementation and launch of NZ Website."
+    }
+  ]
+}; // end work object
+
 var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
       var formattedName = HTMLheaderName.replace("%data%", bio.name);
       var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
