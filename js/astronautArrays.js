6bdf486d36a61a088651381501b9cdf32985d010
zippo-yap <zippo-yap@gmail.com>
2016-04-24 13:49:23 +1200

    Upload answers to challenges

---
+++ js/astronautArrays.js
@@ -0,0 +1,29 @@
+var moonWalkers = [
+    "Neil Armstrong",
+    "Buzz Aldrin",
+    "Pete Conrad",
+    "Alan Bean",
+    "Alan Shepard",
+    "Edgar Mitchell",
+    "David Scott",
+    "James Irwin",
+    "John Young",
+    "Charles Duke",
+    "Eugene Cernan",
+    "Harrison Schmitt"
+];
+
+function alphabetizer(names) {
+    var array = names;
+    var newArray = [];
+    //Loop through each item in the array and split each string by " " and reverse then join back and push to new array.
+    for (var name in array){
+        newArray.push(array[name].split(" ").reverse().join(", "));
+    }
+    // Sort items in array alphabetically
+    newArray.sort();
+    return newArray;
+}
+
+// Try logging your results to test your code!
+console.log(alphabetizer(moonWalkers));
---
+++ js/relationships.js
@@ -0,0 +1,33 @@
+function getRelationship(x, y) {
+    //Declare a variable to be returned later 
+    var statement; 
+    //Compare x to y
+    if (isNaN(x) && isNaN(y)) {
+        statement = "Can't compare relationships because " + x + " and " + y + " are not numbers.";
+    }
+    else if (isNaN(x)) {
+        statement = "Can't compare relationships because " + x + " is not a number.";
+    }
+    else if (isNaN(y)) {
+        statement = "Can't compare relationships because " + y + " is not a number." ;
+    }
+    else if (x < y) {
+        statement = "<";
+    }
+    else if (x > y) {
+        statement = ">";
+    }
+    else if (x === y){
+        statement = "=";
+    }
+      return statement;
+};
+
+console.log(getRelationship(1,4));
+console.log(getRelationship(1,1));
+console.log(getRelationship("that",2));
+console.log(getRelationship("this"," something else"));
+console.log(getRelationship(3));
+console.log(getRelationship("hi"));
+console.log(getRelationship(NaN));
+console.log(getRelationship(NaN, undefined));
